19c09c70e573f80109896237f72a45735bb7d3e3
HussamAldareh <hussamaldareh095@gmail.com>
2026-04-21 22:53:54 +0300

    TasksOOP

---
+++ AdvancLinq/AdvancLinq/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancLinq
+{
+    internal class Program
+    {
+
+
+
+
+        static void Rotat()
+        {
+
+         
+            int[] arr = { 1, 2, 3, 4, 5, };
+            int len = arr.Length;
+            Console.WriteLine("Enter the n");
+            int n = Console.ReadLine().Length;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int last = arr[len - 1];
+                for (int j = len - 1; j > 0; j--)
+                {
+                    arr[j]=arr[j-1];
+                
+                }
+                arr[0] = last;
+
+            }
+            Console.WriteLine(string.Join("",arr));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Rotat();
+
+
+        }
+    }
+}
